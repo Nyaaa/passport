fa2ebfbe242d48e9fc9a7234db143713d8d2f01b
Nyaaa <1457047+Nyaaa@users.noreply.github.com>
2023-02-15 16:16:34 +0300

    refactoring qs for chart data

--- passport/main/charts.py
+++ passport/main/charts.py
@@ -1,34 +1,24 @@
 import matplotlib.pyplot as plt
 from io import StringIO
 from .models import Set, Order
-from django.db.models import OuterRef, Subquery
+from django.db.models import OuterRef, Subquery, Count
 
 
 def get_data():
     qs = Set.objects.all()
-    qs = qs.select_related('article')
     latest_order = Order.objects.filter(sets=OuterRef('pk')).order_by('-date')[:1]
     qs = qs.prefetch_related('order_set').annotate(recipient=Subquery(latest_order.values('recipient__name')),
                                                    distributor=Subquery(latest_order.values('distributor__name')),
-                                                   city=Subquery(latest_order.values('city__name')),
                                                    )
     return qs
 
 
 def distributor_sets_chart():
-    # TODO refactor this
-    qs = get_data().values_list('distributor', 'serial')
-    new_data = {}
-    for e in qs:
-        if e[0] not in new_data:
-            new_data[e[0]] = 1
-        else:
-            new_data[e[0]] += 1
-    new_data['None'] = new_data.pop(None)
-
-    y = list(new_data.keys())
-    x = list(new_data.values())
-    plt.barh(y, x)
+    qs = get_data().values('distributor').order_by('distributor')\
+        .annotate(count=Count('serial')).values_list('distributor', 'count')
+    names, counts = zip(*qs)
+    names = [str(n) for n in names]
+    plt.barh(names, counts)
 
     plt.xlabel("Sets in possession")
     plt.ylabel("Distributors")
