007e87e420d9917ccd5528146147a675033239b4
Nyaaa <1457047+Nyaaa@users.noreply.github.com>
2023-03-12 23:45:37 +0300

    font-awesome -> bootstrap icons

--- passport/static/js/switch.js
+++ passport/static/js/switch.js
@@ -14,17 +14,17 @@ if (settings == null) {
 
 if (settings == 'dark') {
   document.documentElement.setAttribute('data-bs-theme','dark');
-  document.getElementById('lightSwitch').innerHTML = '<i class="fa-regular fa-sun"></i>';
+  document.getElementById('lightSwitch').innerHTML = '<i class="bi bi-sun"></i>';
 }
 
 document.getElementById('lightSwitch').addEventListener('click',()=>{
   if (document.documentElement.getAttribute('data-bs-theme') == 'dark') {
     document.documentElement.setAttribute('data-bs-theme','light');
     localStorage.setItem('lightSwitch', 'light')
-    document.getElementById('lightSwitch').innerHTML = '<i class="fa-regular fa-moon"></i>';
+    document.getElementById('lightSwitch').innerHTML = '<i class="bi bi-moon"></i>';
   } else {
     document.documentElement.setAttribute('data-bs-theme','dark');
     localStorage.setItem('lightSwitch', 'dark')
-    document.getElementById('lightSwitch').innerHTML = '<i class="fa-regular fa-sun"></i>';
+    document.getElementById('lightSwitch').innerHTML = '<i class="bi bi-sun"></i>';
   }
 })
