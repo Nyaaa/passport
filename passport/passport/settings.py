1a1ee9a84d94adc3e50fd1e4ee46bb69e5a39b26
Nyaaa <1457047+Nyaaa@users.noreply.github.com>
2024-01-15 17:02:15 +0300

    version bump

--- passport/passport/settings.py
+++ passport/passport/settings.py
@@ -26,10 +26,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY', 'test')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', 'False')
+DEBUG = False
 
 ALLOWED_HOSTS = [os.getenv('ALLOWED_HOSTS', '127.0.0.1')]
 CSRF_TRUSTED_ORIGINS = [
